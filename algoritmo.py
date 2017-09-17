8ac08498de1de34ec105d72cc01e81bb67853e47
Pi <vini.g.fer@gmail.com>
2017-09-17 18:47:25 -0300

    Adicionada leitura do arquivo e classe Processo

--- algoritmo.py
+++ algoritmo.py
@@ -1,3 +1,21 @@
-file = open("arquivos_para_teste/trab-so1-teste1.txt", "r")
-entrada = file.read()
-print entrada
+class Processo():
+    def __init__(self, dados):
+        dados = dados.split(' ')
+        self.tempo_chegada = int(dados[0])
+        self.tempo_execucao = int(dados[1])
+        self.prioridade = int(dados[2])
+
+
+with open("arquivos_para_teste/trab-so1-teste1.txt", "r") as file:
+    entrada = file.read().split('\n')
+    numero_processos = int(entrada[0])
+    tamanho_fatia_tempo = int(entrada[1])
+
+    print numero_processos
+    print tamanho_fatia_tempo
+    lista_de_processos = []
+    for i in range(0, numero_processos):
+        print entrada[i + 2]
+        lista_de_processos.append(Processo(entrada[i + 2]))
+
+    ordenada_por_chegada = sorted(lista_de_processos, key=lambda x: x.tempo_chegada, reverse=False)
